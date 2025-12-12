438eef5dfc5c5a7aa6d9f6118d50db60ab936de4
Nitin Kumar Shah <shahnitin500@gmail.com>
2025-12-12 11:45:01 +0545

    Initialize the empty project

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace smart_journal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
