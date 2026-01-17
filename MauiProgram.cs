3500d4809d08fa2fac1170bb4bb6c9defacf089f
Nitin Kumar Shah <shahnitin500@gmail.com>
2026-01-17 17:42:18 +0545

    feat: Export journal functionality implemented

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using smart_journal.Data;
+using CommunityToolkit.Maui; // Required for FileSaver
 
 namespace smart_journal
 {
@@ -10,13 +11,16 @@ namespace smart_journal
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                // Initialize the Community Toolkit
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
             builder.Services.AddMauiBlazorWebView();
-            // Register the database as a Singleton (one instance for the whole app)
+
+            // Register the database as a Singleton
             builder.Services.AddSingleton<AppDatabase>();
             builder.Services.AddSingleton<Services.AppState>();
 
